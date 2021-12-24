6ce359d96e71c2381e5102d1a6e9f6fb8e4e7aa7
hans <hmrtn@pm.me>
2021-12-23 18:09:53 -0800

    updated

--- server.go
+++ server.go
@@ -15,32 +15,28 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-var mongoURI = os.Getenv("DATABASE_URL")
-
-var dbCollection = os.Getenv("DATABASE_COLLECTION")
-var dbName = os.Getenv("DATABASE_NAME")
-var port = os.Getenv("PORT")
-
 type MongoInstance struct {
 	Client *mongo.Client
 	DB     *mongo.Database
 }
 
+// Get env variables
+var mongoURI = os.Getenv("DATABASE_URL")
+var dbCollection = os.Getenv("DATABASE_COLLECTION")
+var dbName = os.Getenv("DATABASE_NAME")
+var port = os.Getenv("PORT")
+
 var mg MongoInstance
 
 func init() {
-
 	client, err := mongo.NewClient(options.Client().ApplyURI(mongoURI))
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	ctx, stop := context.WithTimeout(context.Background(), 30*time.Second)
 	defer stop()
-
 	err = client.Connect(ctx)
 	db := client.Database(dbName)
-
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -85,34 +81,27 @@ func GetParty(ctx *fiber.Ctx) error {
 		}
 		return ctx.SendStatus(500)
 	}
-
 	return ctx.Status(200).JSON(party)
 }
 
 // Create a new party
 func NewParty(ctx *fiber.Ctx) error {
 	collection := mg.DB.Collection(dbCollection)
-
 	party := new(models.Party)
-
 	if err := ctx.BodyParser(party); err != nil {
 		return ctx.Status(400).SendString(err.Error())
 	}
 	// ensure mongo always sets generated ObjectIDs
 	party.ID = ""
-
 	insertRes, err := collection.InsertOne(ctx.Context(), party)
 	if err != nil {
 		return ctx.Status(500).SendString(err.Error())
 	}
-
 	filter := bson.D{{Key: "_id", Value: insertRes.InsertedID}}
 	createdRecord := collection.FindOne(ctx.Context(), filter)
 	createdParty := &models.Party{}
 	createdRecord.Decode(createdParty)
-
 	return ctx.Status(200).JSON(createdParty)
-
 }
 
 // Update a party
@@ -135,7 +124,6 @@ func UpdateParty(ctx *fiber.Ctx) error {
 			},
 		},
 	}
-
 	err = mg.DB.Collection(dbCollection).FindOneAndUpdate(ctx.Context(), query, update).Err()
 	if err != nil {
 		if err == mongo.ErrNoDocuments {
@@ -146,7 +134,6 @@ func UpdateParty(ctx *fiber.Ctx) error {
 	// return updated ObjectId
 	party.ID = ctx.Params("id")
 	return ctx.Status(200).JSON(party)
-
 }
 
 // Delete party
@@ -155,40 +142,33 @@ func DeleteParty(ctx *fiber.Ctx) error {
 	partyID, err := primitive.ObjectIDFromHex(
 		ctx.Params("id"),
 	)
-
 	if err != nil {
 		return ctx.SendStatus(400)
 	}
-
 	// find and delete the party with the given ID
 	query := bson.D{{Key: "_id", Value: partyID}}
 	result, err := mg.DB.Collection(dbCollection).DeleteOne(ctx.Context(), &query)
-
 	if err != nil {
 		return ctx.SendStatus(500)
 	}
-
 	if result.DeletedCount < 1 {
 		return ctx.SendStatus(404)
 	}
-
 	// the record was deleted
 	return ctx.SendStatus(204)
 }
 
 func main() {
-
 	// Create Fiber App
 	app := fiber.New()
-
+	// Use CORS
+	// TODO: Configure CORS
 	app.Use(cors.New())
-
+	// App routes
 	app.Get("/parties", GetAllParties)
 	app.Get("/party/:id", GetParty)
 	app.Post("/party", NewParty)
 	app.Put("/party/:id", UpdateParty)
 	app.Delete("/party/:id", DeleteParty)
-
 	log.Fatal(app.Listen(":" + port))
-
 }
